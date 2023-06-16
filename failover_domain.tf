46b7f0195755931f9d15a96d344c4796642eac2d
Cristian Beraha <cristian@cloudops.works>
2023-06-16 15:24:04 -0300

    Failover configuration health checks differ for internal load balancers & resources

--- failover_domain.tf
+++ failover_domain.tf
@@ -9,7 +9,7 @@ data "aws_route53_zone" "failover_app_domain" {
 }
 
 resource "aws_route53_record" "failover_record" {
-  count = var.failover_enabled && !var.app_domain_disabled && !(var.deployment_a_deactivated && var.deployment_a_deactivated) ? 1 : 0
+  count = var.failover_enabled && !var.app_domain_disabled && !(var.deployment_a_deactivated && var.deployment_b_deactivated) && var.load_balancer_public ? 1 : 0
 
   name    = format("%s.%s", var.failover_domain_alias, var.app_domain_name)
   type    = "A"
@@ -25,4 +25,54 @@ resource "aws_route53_record" "failover_record" {
     zone_id                = data.aws_route53_zone.failover_app_domain.id
     evaluate_target_health = true
   }
+}
+
+resource "aws_route53_record" "failover_record_internal" {
+  count = var.failover_enabled && !var.app_domain_disabled && !(var.deployment_a_deactivated && var.deployment_b_deactivated) && !var.load_balancer_public ? 1 : 0
+
+  name    = format("%s.%s", var.failover_domain_alias, var.app_domain_name)
+  type    = "A"
+  zone_id = data.aws_route53_zone.failover_app_domain.id
+
+  failover_routing_policy {
+    type = upper(var.failover_type)
+  }
+  set_identifier  = format("%s-%s-%s", var.release_name, var.namespace, var.region)
+  health_check_id = aws_route53_health_check.health_all.id
+
+  alias {
+    name                   = try(module.app_dns_a[0].fqdn, module.app_dns_b[0].fqdn, "")
+    zone_id                = data.aws_route53_zone.failover_app_domain.id
+    evaluate_target_health = false
+  }
+}
+
+locals {
+  health_check_ids = compact([can(aws_route53_health_check.health_a.id) ? aws_route53_health_check.health_a.id : null, can(aws_route53_health_check.health_b.id) ? aws_route53_health_check.health_b.id : null])
+}
+
+resource "aws_route53_health_check" "health_all" {
+  count = var.failover_enabled && !var.app_domain_disabled && !(var.deployment_a_deactivated && var.deployment_b_deactivated) && !var.load_balancer_public ? 1 : 0
+
+  type                   = "CALCULATED"
+  child_health_threshold = 1
+  child_healthchecks     = local.health_check_ids
+}
+
+resource "aws_route53_health_check" "health_a" {
+  count = var.failover_enabled && !var.app_domain_disabled && !var.deployment_a_deactivated && !var.load_balancer_public ? 1 : 0
+
+  type                            = "CLOUDWATCH_METRIC"
+  cloudwatch_alarm_name           = aws_cloudwatch_metric_alarm.metric_alarm_a.alarm_name
+  cloudwatch_alarm_region         = var.region
+  insufficient_data_health_status = "Unhealthy"
+}
+
+resource "aws_route53_health_check" "health_b" {
+  count = var.failover_enabled && !var.app_domain_disabled && !var.deployment_b_deactivated && !var.load_balancer_public ? 1 : 0
+
+  type                            = "CLOUDWATCH_METRIC"
+  cloudwatch_alarm_name           = aws_cloudwatch_metric_alarm.metric_alarm_b.alarm_name
+  cloudwatch_alarm_region         = var.region
+  insufficient_data_health_status = "Unhealthy"
 }
