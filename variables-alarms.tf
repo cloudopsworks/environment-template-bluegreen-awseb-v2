f2e5f51957fdd392d9d47ab56446d90121538b51
Cristian Beraha <cristian@cloudops.works>
2023-06-21 13:21:34 -0300

    Bump Version -  Tidy up formatting

--- variables-alarms.tf
+++ variables-alarms.tf
@@ -23,13 +23,13 @@ variable "cloudwatch_alarm_threshold" {
 }
 
 variable "cloudwatch_alarm_period" {
-  type = number
-  default = 120
-    description = "(optional) The period for the CloudWatch Alarm. Default is 120."
+  type        = number
+  default     = 120
+  description = "(optional) The period for the CloudWatch Alarm. Default is 120."
 }
 
 variable "cloudwatch_alarm_evaluation_periods" {
-  type = number
-  default = 2
+  type        = number
+  default     = 2
   description = "(optional) The number of evaluation periods for the CloudWatch Alarm. Default is 2."
 }
