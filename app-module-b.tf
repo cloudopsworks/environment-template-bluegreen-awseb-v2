6121a9441ec1b062fb9c40346ae24d073403d6e8
Cristian Beraha <cristian@cloudops.works>
2024-01-25 17:49:49 -0300

    fix: versioning upgrade

--- app-module-b.tf
+++ app-module-b.tf
@@ -32,7 +32,7 @@ module "app_version_b" {
   count = !var.deployment_b_deactivated ? 1 : 0
 
   source          = "cloudopsworks/beanstalk-version/aws"
-  version         = "1.0.7"
+  version         = "1.0.8"
   region          = var.region
   sts_assume_role = var.sts_assume_role
 
